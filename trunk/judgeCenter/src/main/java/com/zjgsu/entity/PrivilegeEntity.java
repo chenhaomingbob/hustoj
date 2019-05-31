6e350707cdc973d1ec7b1d4af564ac2dae505a15
chenhaomingbob <chenhaomingbob@163.com>
2019-05-31 21:13:54 +0800

    添加Entity

---
+++ trunk/judgeCenter/src/main/java/com/zjgsu/entity/PrivilegeEntity.java
@@ -0,0 +1,78 @@
+package com.zjgsu.entity;
+
+import javax.persistence.*;
+
+/**
+ * @author chm 神魔法
+ * @date 2019/5/31 21:10
+ */
+@Entity
+@Table(name = "privilege", schema = "jol", catalog = "")
+public class PrivilegeEntity {
+    private String userId;
+    private String rightstr;
+    private String defunct;
+
+    @Basic
+    @Column(name = "user_id")
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "rightstr")
+    public String getRightstr() {
+        return rightstr;
+    }
+
+    public void setRightstr(String rightstr) {
+        this.rightstr = rightstr;
+    }
+
+    @Basic
+    @Column(name = "defunct")
+    public String getDefunct() {
+        return defunct;
+    }
+
+    public void setDefunct(String defunct) {
+        this.defunct = defunct;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PrivilegeEntity that = (PrivilegeEntity) o;
+
+        if (userId != null ? !userId.equals(that.userId) : that.userId != null) return false;
+        if (rightstr != null ? !rightstr.equals(that.rightstr) : that.rightstr != null) return false;
+        if (defunct != null ? !defunct.equals(that.defunct) : that.defunct != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = userId != null ? userId.hashCode() : 0;
+        result = 31 * result + (rightstr != null ? rightstr.hashCode() : 0);
+        result = 31 * result + (defunct != null ? defunct.hashCode() : 0);
+        return result;
+    }
+
+    private String id;
+
+    @Id
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
---
+++ trunk/judgeCenter/src/main/java/com/zjgsu/entity/SourceCodeEntity.java
@@ -0,0 +1,54 @@
+package com.zjgsu.entity;
+
+import javax.persistence.*;
+
+/**
+ * @author chm 神魔法
+ * @date 2019/5/31 21:10
+ */
+@Entity
+@Table(name = "source_code", schema = "jol", catalog = "")
+public class SourceCodeEntity {
+    private int solutionId;
+    private String source;
+
+    @Id
+    @Column(name = "solution_id")
+    public int getSolutionId() {
+        return solutionId;
+    }
+
+    public void setSolutionId(int solutionId) {
+        this.solutionId = solutionId;
+    }
+
+    @Basic
+    @Column(name = "source")
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SourceCodeEntity that = (SourceCodeEntity) o;
+
+        if (solutionId != that.solutionId) return false;
+        if (source != null ? !source.equals(that.source) : that.source != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = solutionId;
+        result = 31 * result + (source != null ? source.hashCode() : 0);
+        return result;
+    }
+}
